1a6caab29bcedea274e065c25a789b13d2bf7fea
kcao20 <kcao2022@gmail.com>
2024-01-24 12:18:17 -0500

    Overhual CourseDatabase
    Ensure there is only one instance of CourseDatabase running.
    
    Remove previous references to database initialization in MainActivity.
    Now each fragment can grab the singular static instance of the db.

--- app/src/main/java/com/example/collegescheduler/db/CourseDatabase.java
+++ app/src/main/java/com/example/collegescheduler/db/CourseDatabase.java
@@ -1,11 +1,25 @@
 package com.example.collegescheduler.db;
 
-import androidx.room.Dao;
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Course.class}, version = 1)
 public abstract class CourseDatabase extends RoomDatabase {
+
+    private static CourseDatabase instance;
+
+    // Ensure there is only one instance of database
+    public static synchronized CourseDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context.getApplicationContext(), CourseDatabase.class, "CourseDB")
+                    // Add any callbacks or migration strategies if needed
+                    .fallbackToDestructiveMigration().build();
+        }
+        return instance;
+    }
 
     public abstract CourseDAO getCourseDao();
 
