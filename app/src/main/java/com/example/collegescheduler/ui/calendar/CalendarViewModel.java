cc2a80ca3027cdc6b3ab07b94e4a3282875e4f3f
unknown <rayy12204@gmail.com>
2024-01-26 14:59:13 -0500

    Initilized Custom Calendar
    -Created a basic calendar that displays a calendar in a 6x7 grid
    -Each cell in the grid is modifiable, allowing for potential events (Schedule/Assignments/To-Do-List stuff)
    -There is a button that brings you to the current month and day.
    -Two buttons to add/subtract a month.

--- app/src/main/java/com/example/collegescheduler/ui/calendar/CalendarViewModel.java
+++ app/src/main/java/com/example/collegescheduler/ui/calendar/CalendarViewModel.java
@@ -1,16 +1,16 @@
-package com.example.collegescheduler.ui.gallery;
+package com.example.collegescheduler.ui.calendar;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class CalendarViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public CalendarViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("My Calendar");
     }
 
     public LiveData<String> getText() {
