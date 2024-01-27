293cd21f7e6719f171245656a801774be46f8387
kcao20 <kcao2022@gmail.com>
2024-01-26 22:44:36 -0500

    Create GitHub action to auto build app

--- app/src/main/java/com/example/collegescheduler/db/CourseDatabase.java
+++ app/src/main/java/com/example/collegescheduler/db/CourseDatabase.java
@@ -6,7 +6,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Course.class}, version = 2)
+@Database(entities = {Course.class}, version = 2, exportSchema = false)
 public abstract class CourseDatabase extends RoomDatabase {
 
     private static CourseDatabase instance;
